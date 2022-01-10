35c03a70ea59e234670a3249529391f33114ffc3
Ashish <ath026@gmail.com>
2022-01-10 09:52:24 -0600

    separated CheckProcessingStatus.java for running processing timeline search independent of existing suite
    Signed-off-by: Ashish <ath026@gmail.com>

---
+++ src/test/java/com/caseStatus/testCases/CheckProcessingStatus.java
@@ -0,0 +1,31 @@
+package com.caseStatus.testCases;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import com.caseStatus.pageObjects.ProcessingTimesPage;
+
+public class CheckProcessingStatus extends BaseTestCase {
+	
+	@Test
+	
+	public void checkCaseProcessingTimes() {
+		driver.navigate().to("https://egov.uscis.gov/processing-times/");
+		ProcessingTimesPage ptp=new ProcessingTimesPage(driver);
+		Select form =new Select(ptp.selectForm);
+		form.selectByVisibleText("I-765 | Application for Employment Authorization");
+		Select office =new Select(ptp.selectOffice);
+		office.selectByVisibleText("Potomac Service Center");		
+		ptp.getProcessingtime.click();
+		WebDriverWait wait=new WebDriverWait(driver,10);
+		wait.until(ExpectedConditions.visibilityOf(ptp.formdesc));
+		wait.until(ExpectedConditions.visibilityOf(ptp.runningdate));
+		String running=ptp.runningdate.getText();
+		text+="*****Potomac Processing***************\n";
+		text+="Based on a pending asylum: "+running+"\n\n";
+	}
+	
+
+}
